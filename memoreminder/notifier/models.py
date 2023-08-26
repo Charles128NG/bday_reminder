f7a9ce2f5811e9491eb4180fc2d78ea7ac0864c2
Aryan <negiaryan53@gmail.com>
2023-08-26 16:25:07 +0530

    build models and the basic structures

---
+++ memoreminder/notifier/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Notification(models.Model):
+    Name = models.CharField(max_length=150)
+    Occasion = models.CharField(max_length=100)
+    Date = models.DateField()
+
+    def __str__(self):
+        return self.Name
